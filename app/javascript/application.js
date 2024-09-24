d374936e36d7e396808905eacb3b1d606f971a4c
Bui The Hoa <buithehoa@gmail.com>
2024-09-24 10:59:03 +0700

    Put flatpickr in its own controller.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,14 +1,4 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 
 import { Turbo } from "@hotwired/turbo-rails";
-import flatpickr from "flatpickr";
 import "controllers"
-
-document.addEventListener("turbo:load", () => {
-    console.log('turbo loaded');
-
-    flatpickr(".datetimepicker", {
-        enableTime: true,
-        dateFormat: "Y-m-d H:i",
-    });
-});
